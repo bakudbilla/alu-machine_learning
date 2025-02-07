a9869f967e6b1970600a575c062f7cfb198bf952
Bernice <berniceawinpang@gmail.com>
2025-02-07 17:10:14 +0200

    tasks

--- supervised_learning/regularization/2-l2_reg_cost.py
+++ supervised_learning/regularization/2-l2_reg_cost.py
@@ -6,7 +6,13 @@ import tensorflow as tf
 
 def l2_reg_cost(cost):
     """
-    Function to calculate the L2 regularization in tf 
+    Function to calculate the L2 regularization in tf
+    Args:
+        cost: Tensor containing the cost of the network without
+              L2 regularization
+
+    Returns: Tensor containing the cost of the network accounting
+             for L2 regularization
 
     """
 
