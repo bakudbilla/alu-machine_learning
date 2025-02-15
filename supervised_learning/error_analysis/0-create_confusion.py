e99366737035d0c48143c1b20bbb22393516317a
Bernice <berniceawinpang@gmail.com>
2025-02-15 19:42:00 +0200

    task 0

--- supervised_learning/error_analysis/0-create_confusion.py
+++ supervised_learning/error_analysis/0-create_confusion.py
@@ -11,6 +11,8 @@ def create_confusion_matrix(labels, logits):
     m is the number of data points
     classes is the number of classes
     logits is a one-hot numpy.ndarray of shape (m, classes) containing the predicted labels 
-    Returns: a confusion numpy.ndarray of shape (classes, classes) with row indices representing t he correct labels and column indices representing the predicted labels
+    Returns: a confusion numpy.ndarray of shape (classes, classes) with row indices representing
+    the correct labels and column indices representing the predicted labels
+
     """
     return np.matmul(labels.T, logits)
