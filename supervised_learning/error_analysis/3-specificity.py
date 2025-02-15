6dffd59024f545a5c1711abaf7979ae45ccbdf40
Bernice <berniceawinpang@gmail.com>
2025-02-15 19:53:49 +0200

    error analysis

---
+++ supervised_learning/error_analysis/3-specificity.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""Script to calculate the specificity in a
+    confusion matrix
+"""
+
+import numpy as np
+
+
+def specificity(confusion):
+    """
+    Function to calculate the specificity
+    Args:
+        confusion: numpy.ndarray of shape
+                    (classes, classes)
+    """
+    TP = np.diag(confusion)
+    FP = np.sum(confusion, axis=0) - TP
+    FN = np.sum(confusion, axis=1) - TP
+    TN = np.sum(confusion) - (FP + FN + TP)
+
+    TNR = TN / (TN + FP)
+    return TNR
