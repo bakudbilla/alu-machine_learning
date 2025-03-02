d8fa4a7a41b49d223ac4094ef78ee41cecd607de
Bernice <berniceawinpang@gmail.com>
2025-03-02 23:42:14 +0200

    3

--- pipeline/apis/3-upcoming.py
+++ pipeline/apis/3-upcoming.py
@@ -1,23 +1,40 @@
 #!/usr/bin/env python3
+"""
+Uses the (unofficial) SpaceX API to print the upcoming launch as:
+<launch name> (<date>) <rocket name> - <launchpad name> (<launchpad locality>)
 
-"""Script that prints the location of a specific user"""
+The “upcoming launch” is the one which is the soonest from now, in UTC
+and if 2 launches have the same date, it's the first one in the API result.
+"""
+
 
 import requests
-import sys
-import time
 
 
 if __name__ == "__main__":
-    res = requests.get(sys.argv[1])
+    url = "https://api.spacexdata.com/v4/launches/upcoming"
+    results = requests.get(url).json()
+    dateCheck = float('inf')
+    launchName = None
+    rocket = None
+    launchPad = None
+    location = None
+    for launch in results:
+        launchDate = launch.get('date_unix')
+        if launchDate < dateCheck:
+            dateCheck = launchDate
+            date = launch.get('date_local')
+            launchName = launch.get('name')
+            rocket = launch.get('rocket')
+            launchPad = launch.get('launchpad')
+    if rocket:
+        rocket = requests.get('https://api.spacexdata.com/v4/rockets/{}'.
+                              format(rocket)).json().get('name')
+    if launchPad:
+        launchpad = requests.get('https://api.spacexdata.com/v4/launchpads/{}'.
+                                 format(launchPad)).json()
+        launchPad = launchpad.get('name')
+        location = launchpad.get('locality')
 
-    if res.status_code == 403:
-        rate_limit = int(res.headers.get('X-Ratelimit-Reset'))
-        current_time = int(time.time())
-        diff = int((rate_limit - current_time) / 60)
-        print('Reset in {} min'.format(int(diff)))
-
-    elif res.status_code == 404:
-        print("Not found")
-    elif res.status_code == 200:
-        res = res.json()
-        print(res['location'])
+    print("{} ({}) {} - {} ({})".format(
+        launchName, date, rocket, launchPad, location))
