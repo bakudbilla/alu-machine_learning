62255d0874a8aefdc35ab90019c506f028b28c5a
Bernice <berniceawinpang@gmail.com>
2025-03-09 15:03:03 +0200

    task 16 fixed

--- pipeline/databases/16-glam_rock.sql
+++ pipeline/databases/16-glam_rock.sql
@@ -1,5 +1,5 @@
--- query to list only one genre ranked by their longevity
-SELECT band_name, IF(split IS NULL, YEAR(CURDATE()), split) - formed AS lifespan
+-- script that lists all bands with Glam rock  ranked by their longevity
+SELECT band_name, IF(split IS NULL, 2020, split) - formed AS lifespan
 FROM metal_bands
 WHERE style LIKE "%Glam Rock%"
 ORDER BY lifespan DESC;
