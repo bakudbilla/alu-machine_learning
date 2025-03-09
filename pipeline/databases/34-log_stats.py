5544cde2b47e8d3fa5cf97ecf36214cacf2f9b11
Bernice <berniceawinpang@gmail.com>
2025-03-09 14:26:53 +0200

    task 34

--- pipeline/databases/34-log_stats.py
+++ pipeline/databases/34-log_stats.py
@@ -5,4 +5,18 @@ from pymongo import MongoClient
 
 
 if __name__ == "__main__":
-    print("{} status check".format())
+    client = MongoClient('mongodb://localhost:27017/')
+    db = client['logs']
+    collection = db['nginx']
+
+    total_logs = collection.count_documents({})
+    print(f"{total_logs} logs")
+
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    print("Methods:")
+    for method in methods:
+        count = collection.count_documents({"method": method})
+        print(f"    {method} {count}")
+
+    status_check_count = collection.count_documents({"method": "GET", "path": "/status"})
+    print(f"{status_check_count} status check")
