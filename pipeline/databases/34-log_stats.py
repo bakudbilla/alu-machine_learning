54298866d009e1833b51c5a8ed4674c313dd91e4
Bernice Akudbilla <b.akudbilla@alusstudent.com>
2025-03-09 05:50:23 -0700

    chmod

--- pipeline/databases/34-log_stats.py
+++ pipeline/databases/34-log_stats.py
@@ -1,22 +1,22 @@
 #!/usr/bin/env python3
-""" Log stats """
+"""
+script that provides some stats about Nginx logs stored in MongoDB
+"""
+
 
 from pymongo import MongoClient
 
 
 if __name__ == "__main__":
-    client = MongoClient('mongodb://localhost:27017/')
-    db = client['logs']
-    collection = db['nginx']
-
-    total_logs = collection.count_documents({})
-    print(f"{total_logs} logs")
-
-    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    logs_collection = client.logs.nginx
+    doc_count = logs_collection.count_documents({})
+    print("{} logs".format(doc_count))
     print("Methods:")
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
     for method in methods:
-        count = collection.count_documents({"method": method})
-        print(f"    {method} {count}")
-
-    status_check_count = collection.count_documents({"method": "GET", "path": "/status"})
-    print(f"{status_check_count} status check")
+        method_count = logs_collection.count_documents({"method": method})
+        print("\tmethod {}: {}".format(method, method_count))
+    path = {"method": "GET", "path": "/status"}
+    path_count = logs_collection.count_documents(path)
+    print("{} status check".format(path_count))
